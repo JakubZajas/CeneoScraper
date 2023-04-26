94431f7610506f23c1db31a9c497786271a2a4c5
JakubZajas <225983@student.uek.krakow.pl>
2023-04-26 13:01:47 +0200

    2023-04-26 JZ

--- analyser.py
+++ analyser.py
@@ -1,17 +1,62 @@
 import os
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
 print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="\n")
 
 product_code=input("Podaj  kod produktu: ")
-
 opinions = pd.read_json(f"./opinions/{product_code}.json")
+#opinions.score = opinions.scoer.map(lambda x: float(x[:-2].replace(",",".")))
+opinions.score = opinions.score.map(lambda x: float(x.split("/")[0].replace(",",".")))
+
 
 opinions_count = len(opinions.index)
 #pros_count = sum([False if len(p)==0 else True for p in opinions.pros])
-pros_count=opinions.pros.map(lambda p: False if len(p)==0 else True ).sum()
 #cons_count = sum([False if len(c)==0 else True for c in opinions.cons])
-cons_count=opinions.cons.map(lambda c: False if len(c)==0 else True ).sum()
-avg_score = 0
+#pros_count=opinions.pros.map(lambda p: False if len(p)==0 else True ).sum()
+#cons_count=opinions.cons.map(lambda c: False if len(c)==0 else True ).sum()
+pros_count=opinions.pros.map(bool).sum()
+cons_count=opinions.cons.map(bool).sum()
+#avg_score = opinions.scoer.map(lambda x: float(x.split("/")[0].replace(",","."))).mean()
+avg_score = opinions.score.mean()
 
-print(cons_count)
+print(
+    f'''Dla produktu  o kodzie: {product_code} dostepnych jest {opinions_count} opinii.
+    Dla {pros_count} opinni dostepna jest  lista  zalet,
+    a dla {cons_count} dostepna jest liczb wad.
+    Srednia  ocena produktu to {avg_score.round(2)}.'''
+)
+
+# histogram czestosci wystepowania poszczegolnych ocen
+score= opinions.score.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value = 0)
+
+score.plot.bar(color="hotpink")
+plt.xticks(rotation=0)
+plt.title("Histogram ocen")
+plt.xlabel("Liczba gwiazdek")
+plt.ylabel("Liczba opinii")
+for index, value in enumerate(score):
+    plt.text(index, value+0.5, str(value) , ha="center")
+
+#plt.show()
+
+try:
+    os.mkdir("./plots")
+except FileExistsError:
+   pass
+
+plt.savefig(f"./plots/{product_code}_score.png")
+plt.close()
+
+#udzial poszczegolnych rekomendacji w ogolnej liczbie opinii
+recommendation = opinions["recommendation"].value_counts(dropna=False).sort_index()
+print(recommendation)
+recommendation.plot.pie(label="", 
+                        autopct="%1.1f%%",
+                        labels=["Nie polecam","Polceam","Nie mam zdania"],
+                        colors=["crimson","forestgreen","gray"])
+
+plt.legend(bbox_to_anchor=(1.0,1.0))
+plt.savefig(f"./plots/{product_code}_recommendation.png")
+plt.close()
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests 
 import json
+import os 
 from bs4 import BeautifulSoup
 
 
@@ -49,6 +50,10 @@ while(url):
         url = None
 
 print(len(all_opinions))
-with open(f"./opinions/{product_code}.json","w",encoding="UTF-8") as  jf:
-   json.dump(all_opinions, jf, indent=4, ensure_ascii= False )
+try:
+    os.mkdir("./opinions")
+except FileExistsError:
+   pass
 
+with open(f"./opinions/{product_code}.json","w",encoding="UTF-8") as  jf:
+        json.dump(all_opinions, jf, indent=4, ensure_ascii= False )
